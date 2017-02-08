6163c4378d0c61965b7fb3bc3e25c1743118a4f3
angelyaaaaa <tsoen.526018@gmail.com>
2017-02-08 10:10:45 +0800

    add chat response buffer

--- src/components/Main.js
+++ src/components/Main.js
@@ -41,11 +41,18 @@ class AppComponent extends React.Component {
 			.then(response => response.json() )
 			.then(data => {
 				const newCount = ++this.state.count;
-				const newChat = {id: newCount, role: 'others', text: data.text}
+				let newChat = {id: newCount, role: 'others', text: '...'}
 				const newChatList = this.state.chatList;
 				newChatList.push(newChat);
 
 				this.setState({chatList: newChatList, count: newCount});
+
+				setTimeout(()=> { 
+					console.log(this);
+					console.log(newCount);
+					newChatList[newCount-1] = {id: newCount, role: 'others', text: data.text};
+					this.setState({chatList: newChatList, count: newCount});
+				}, 1000);
 			});
 	}
 
